5cffc376415d728479bad9935c5adcdcc571449a
Anastasia Bennett <anastasia.n.bennett@gmail.com>
2015-10-19 20:35:36 -0700

    early makings of models

---
+++ js/models.js
@@ -0,0 +1,14 @@
+var BFHVModels = (function() {
+  // var UserModel = Backbone.Model.extend({
+  //   defaults:{
+  //     paswword:'',
+  //     user:''
+  //   }
+  // });
+
+
+// return {
+//     UserModel: UserModel
+// };
+
+})();
