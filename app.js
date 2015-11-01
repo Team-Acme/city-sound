4174ae7cd413f3c25330a8438988cb951a5154c6
ELIZABETH <elizabethkdeering@gmail.com>
2015-11-01 14:28:44 -0800

    fixed module conflicts

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars'),
-var methodOverride = require('method-override'),
-var session = require('express-session'),
-var passport = require('passport'),
-var LocalStrategy = require('passport-local'),
-var TwitterStrategy = require('passport-twitter'),
-var GoogleStrategy = require('passport-google'),
+var hbs = require('hbs');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var TwitterStrategy = require('passport-twitter');
+var GoogleStrategy = require('passport-google');
 var FacebookStrategy = require('passport-facebook');
 
 
@@ -18,7 +18,11 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+//===============PASSPORT===============
 
+//This section will contain our work with Passport
+
+//===============EXPRESS================
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,6 +36,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', routes);
 app.use('/users', users);
@@ -66,6 +75,24 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+// Session-persisted message middleware
+app.use(function(req, res, next){
+  var err = req.session.error,
+      msg = req.session.notice,
+      success = req.session.success;
+
+  delete req.session.error;
+  delete req.session.success;
+  delete req.session.notice;
+
+  if (err) res.locals.error = err;
+  if (msg) res.locals.notice = msg;
+  if (success) res.locals.success = success;
+
+  next();
+});
+
 
 
 module.exports = app;
