359df69be4de5c032783016fd73669f100eeabd9
ELIZABETH <elizabethkdeering@gmail.com>
2015-11-07 15:38:13 -0800

    mored css and changed to dropdown cities

--- public/js/views.js
+++ public/js/views.js
@@ -62,7 +62,7 @@ var GUI = (function() { //IIFE for all Views
     }
   });
 
-  
+
   //////////////////////////////////////////////////////////////////////////////
 
   //NOTES FOR NewPLaylistView:
@@ -104,9 +104,9 @@ var GUI = (function() { //IIFE for all Views
       label = '<h2>Bands from where?</h2>';
       console.log("NamedPlaylistView render is listening");
       // this.$el.html(label);
-      var cityName = '<input type="text" id="city-Name">';
-      var saveCityBtn = '<div id="saveCity">Go!</div>';
-      this.$el.html(label + '<b>' + "City: " + '</b>' + cityName + '</br>' + '</b>' + '</b>' + '<b>' + saveCityBtn);
+      var cityName = '<select class="citiesList"><option value="Portland">Portland</option><option value="Seattle">Seattle</option><option value="Minneapolis">Minneapolis</option><option value="Austin">Austin</option><option value="Cleveland">Cleveland</option></select>';
+      var saveCityBtn = '<div id="saveCity"><img src="/img/monster_black.png"/></div>';
+      this.$el.html(label + cityName + saveCityBtn);
     }
   });
 
