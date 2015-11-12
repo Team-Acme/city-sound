8167f8458301a18be5a1a1d401f257732b3c3208
ELIZABETH <elizabethkdeering@gmail.com>
2015-11-11 20:38:40 -0800

    css changes

--- public/js/views.js
+++ public/js/views.js
@@ -99,12 +99,12 @@ var GUI = (function() { //IIFE for all Views
     },
 
     render: function() {
-      label = '<h2>Bands from where?  City:</h2>';
+      label = '<h2>Bands from where?</h2>';
       console.log("NamedPlaylistView render is listening");
       // this.$el.html(label);
 
 
-      var cityName = '<select id="citiesList"><option value=""></option><option value="Portland">Portland</option><option value="Seattle">Seattle</option><option value="Minneapolis">Minneapolis</option><option value="Austin">Austin</option><option value="Cleveland">Cleveland</option></select>';
+      var cityName = '<select id="citiesList" class="citySelects"><option value="">choose city</option><option value="Portland">Portland</option><option value="Seattle">Seattle</option><option value="Minneapolis">Minneapolis</option><option value="Austin">Austin</option><option value="Cleveland">Cleveland</option></select>';
       //var saveCityBtn = '<div id="saveCity"><img src="/img/monster_black.png"/></div>';
       var buttons = '<button id="logout">log out</button>';
       closeDiv = '</div>';
