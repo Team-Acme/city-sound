523f2c3c23dd041a05b0b783b0df37ae98117788
Anastasia Bennett <anastasia.n.bennett@gmail.com>
2015-11-16 08:22:39 -0800

    deleted Iife

--- public/js/models.js
+++ public/js/models.js
@@ -1,7 +1,7 @@
 
 console.log("models.js is loading");
 
-var BFHModels = (function() {
+// var BFHModels = (function() {
   var UserModel = Backbone.Model.extend({
     defaults:{
       username:'',
@@ -20,7 +20,7 @@ var BFHModels = (function() {
 
   var Posts = Backbone.Collection.extend({
     model: PostModel,
-    url:'/posts',
+    url:'/posts/',
     initialize: function (){
       this.fetch();
     }
@@ -35,11 +35,12 @@ var BFHModels = (function() {
     }
   });
 
-  return {
-    UserModel: UserModel,
-    PostModel: PostModel,
-    Posts: Posts,
-    UsersPosts: UsersPosts
-  }
 
-})();
+  // return {
+  //   UserModel: UserModel,
+  //   PostModel: PostModel,
+  //   Posts: Posts,
+  //   UsersPosts: UsersPosts
+  // }
+
+// })();
